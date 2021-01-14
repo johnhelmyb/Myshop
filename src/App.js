98072b5d679834c7c9cb6d74ec07343304b73a98
John <52695596+johnhelmyb@users.noreply.github.com>
2021-01-14 22:04:37 +0100

    share code on github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div classNameName="grid-container">
+    <header className="row">
+      <div>
+        <Link className="brand" to="/">MyShop</Link>
+      </div>
+      <div>
+        <Link to="/cart">Panier</Link>
+        <Link to="/signin">Se connecter</Link>
+      </div>
+    </header>
+    <main>
+      <div>
+        <div className="row center">
+          {data.products.map((product) => (
+             <div key={product._id} className="card">
+               <Link to={`/product/${product._id}`}>
+               <img className="medium" src={product.image} alt={product.name} />
+               </Link>
+            <div className="card-body">
+                <Link to="product.html"><h2>{product.name}</h2></Link>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">€{product.price}</div>
+            </div>
+            </div>
+           ))}
+        </div>
+      </div>
+    </main>
+    <footer className="row center">Conditions generals</footer>
+  </div>
   );
 }
 
