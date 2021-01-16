9333f9f8c19bd7c04bf3eb7d8b9e1340647840e3
John <52695596+johnhelmyb@users.noreply.github.com>
2021-01-16 12:59:04 +0100

    new axios and proxy

--- src/Page/HomePage.js
+++ src/Page/HomePage.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Product from '../components/product';
-import data from '../data';
 
 export default  function HomePage() {
+    const [products, setProduct] = useState([]);
+    useEffect(() => {
+       const fetchdata = async () => {
+           const { data } = await axios.get('/api/products');
+           setProduct(data);
+       };
+        fetchdata();
+    }, []);
     return(
         <div>
             <div className="row center">
-                {data.products.map((product) => (
+                {products.map((product) => (
                     <Product key={product._id} product={product}></Product>
                 ))}
             </div>
